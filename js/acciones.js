0b65c068516a70a622113da4a803e342a05232be
Alberto Rivera <albertorivera581@gmail.com>
2014-08-19 14:35:24 -0500

    nada
    nada

--- js/acciones.js
+++ js/acciones.js
@@ -6,22 +6,17 @@ $(document).ready(function(e){
     audio = window.plugin.LowLatencyAudio;
 	audio.preloadFX('do','audio/DO.mp3',function(){}, function(e){alert("Error " +e);});
 	
-	 audio = window.plugin.LowLatencyAudio;
+	 
 	 audio.preloadFX('re','audio/RE.mp3',function(){}, function(e){alert("Error " +e);});
 	
-	 audio = window.plugin.LowLatencyAudio;
 	audio.preloadFX('mi','audio/MI.mp3',function(){}, function(e){alert("Error " +e);});
 	
-	 audio = window.plugin.LowLatencyAudio;
 	audio.preloadFX('fa','audio/FA.mp3',function(){}, function(e){alert("Error " +e);});
 	
-	 audio = window.plugin.LowLatencyAudio;
 	audio.preloadFX('sol','audio/SOL.mp3',function(){}, function(e){alert("Error " +e);});
 	
-	 audio = window.plugin.LowLatencyAudio;
 	audio.preloadFX('la','audio/LA.mp3',function(){}, function(e){alert("Error " +e);});
 	
-	 audio = window.plugin.LowLatencyAudio;
 	audio.preloadFX('si','audio/SI.mp3',function(){}, function(e){alert("Error " +e);});
 	
 	$('.nota').bind('touchstart',function(){
